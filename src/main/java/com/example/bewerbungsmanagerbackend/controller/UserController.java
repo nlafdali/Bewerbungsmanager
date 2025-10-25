4aaf09e063668a286fdfe7fb27125ff97a31227c
nlafdali <lafdali.nadia7@gmail.com>
2025-10-25 20:06:59 +0200

    Initial Spring Boot setup

--- src/main/java/com/example/bewerbungsmanagerbackend/controller/UserController.java
+++ src/main/java/com/example/bewerbungsmanagerbackend/controller/UserController.java
@@ -1,4 +1,39 @@
 package com.example.bewerbungsmanagerbackend.controller;
 
+import com.example.bewerbungsmanagerbackend.model.User;
+import com.example.bewerbungsmanagerbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*") //Für Frontend (z.B. localhost: 8080)
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        try{
+            User saveUser = userService.registerUser(user);
+            return ResponseEntity.ok(saveUser);
+        }catch(RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody User user){
+
+        try{
+            User loggedInUser = userService.loginUSer(user.getEmail(), user.getPassword());
+            return ResponseEntity.ok(loggedInUser);
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
 }
--- src/main/java/com/example/bewerbungsmanagerbackend/service/UserService.java
+++ src/main/java/com/example/bewerbungsmanagerbackend/service/UserService.java
@@ -1,4 +1,42 @@
 package com.example.bewerbungsmanagerbackend.service;
 
+import com.example.bewerbungsmanagerbackend.model.User;
+import com.example.bewerbungsmanagerbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.util.Optional;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    public User registerUser(User user){
+        //prüfen,ob Email schon existiert
+        Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
+        if (existingUser.isPresent()) {
+            throw new RuntimeException("E-Mail-Adresse ist bereits registriert!!");
+        }
+        // Passwort verschlüsseln
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public User loginUSer(String email, String password){
+        Optional<User> existingUser = userRepository.findByEmail(email);
+        if(existingUser.isEmpty()){
+            throw new RuntimeException("Email-Adresse nicht gefunden");
+        }
+        User user = existingUser.get();
+
+        System.out.println("DB-Passwort: " + user.getPassword());
+        System.out.println("Eingegeben: " + password);
+        if (!passwordEncoder.matches(password, user.getPassword())){
+            throw new RuntimeException("Falsches Passwort!!");
+        }
+        return user;
+    }
+
 }
